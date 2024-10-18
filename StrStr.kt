9303f29d5abe2b8781c39609482da9a797b99e65
uitangclan <uitangclan@gmail.com>
2024-10-19 05:09:17 +0900

    happy coding

---
+++ StrStr.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val haystack = "sadbutsad"
+    val needle = "sad"
+    // haystack = "leetcode"
+    // needle = "leeto"
+    println(strStr(haystack, needle))
+}
+
+fun strStr(haystack: String, needle: String): Int {
+    return 404
+}
