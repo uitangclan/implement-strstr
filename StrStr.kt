73cfb63c1b0bcfbb9c988439eb79aca966214753
uitangclan <uitangclan@gmail.com>
2024-10-19 11:31:28 +0900

    happy coding

--- StrStr.kt
+++ StrStr.kt
@@ -1,11 +1,23 @@
 fun main() {
-    val haystack = "sadbutsad"
-    val needle = "sad"
-    // haystack = "leetcode"
-    // needle = "leeto"
+    // val haystack = "sadbutsad"
+    // val needle = "sad"
+    val haystack = "leetcode"
+    val needle = "leeto"
+    // val haystack = "hello"
+    // val needle = "ll"
     println(strStr(haystack, needle))
 }
 
 fun strStr(haystack: String, needle: String): Int {
-    return 404
+    val s = haystack.length
+    val n = needle.length
+    for (windowStart in 0..s - n) {
+        for (i in 0 until n) {
+            if (haystack[windowStart + i] != needle[i]) break
+            if (i == n -1) {
+                return windowStart
+            }
+        }
+    }
+    return -1
 }
